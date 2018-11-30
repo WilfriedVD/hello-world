af84aaaba6b1970ada840fbcf14f10fae76a3331
Wilfried Van Damme <wilfried@timeflies.be>
2018-11-30 20:39:08 +0100

    toevoegen

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
+<?php
+
+$hello = "Hello, world!";
+
+?>
+
+
 <html>
+
+
 
    <head>
       <title>Hello World</title>
    </head>
 
    <body>
-      <?php echo "Hello, world!";?>
+
+      <?php echo $hello ?>
+
+
+      <address>
+        Copyright &copy; <?php echo date("Y"); ?>
+      </address>
    </body>
 
 </html>
